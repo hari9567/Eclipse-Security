2c6463835301382d77ddfdf98ced61934e3a2096
hari9567 <141563918+hari9567@users.noreply.github.com>
2023-08-06 19:07:47 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,44 @@
+class EclipseCore:
+    def __init__(self):
+        # Initialize core components
+        pass
+
+    def do_something(self):
+        # Core functionality
+        pass
+
+class DataHandler:
+    def __init__(self):
+        # Initialize data storage
+        pass
+
+    def save_data(self, data):
+        # Save data to the database
+        pass
+
+    def get_data(self, data_id):
+        # Retrieve data from the database
+        pass
+
+# Usage example:
+data_handler = DataHandler()
+data_handler.save_data({"key": "value"})
+
+
+from sklearn.linear_model import LinearRegression
+
+# Sample data
+X = [[1], [2], [3], [4], [5]]
+y = [2, 4, 5, 4, 5]
+
+# Create a Linear Regression model
+model = LinearRegression()
+
+# Fit the model to the data
+model.fit(X, y)
+
+# Make predictions
+new_data = [[6], [7], [8]]
+predictions = model.predict(new_data)
+print(predictions)
+
